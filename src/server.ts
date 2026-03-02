0f4bed686cc3bd4009b87d804a613a6c53d8227e
Kartik Singh <kartiksingh3337@gmail.com>
2026-03-02 11:19:55 +0530

    final check

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { identifyContact } from "./identity.service";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Parse incoming JSON request bodies
 app.use(express.json());
